a939b031937f625e714db7fd4adcd454275b1498
JacobXXI <jacobphhj@gmail.com>
2025-08-31 20:50:02 +1000

    GH deploy T2

--- braingrow-ai/src/main.tsx
+++ braingrow-ai/src/main.tsx
@@ -4,7 +4,7 @@ import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // Disable StrictMode during dev to avoid double effects/fetches
-  <BrowserRouter basename="/braingrow-ai">
+  <BrowserRouter basename="/BraingrowAI">
     <App />
   </BrowserRouter>
 );
